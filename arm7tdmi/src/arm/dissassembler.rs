d78057cf3f0285df0ffcbc60893972d3eb8a1423
Aza Walker <azawalker.jr@gmail.com>
2024-07-16 21:09:32 -0400

    finished arm instruction dissabler

--- arm7tdmi/src/arm/dissassembler.rs
+++ arm7tdmi/src/arm/dissassembler.rs
@@ -9,22 +9,25 @@ pub enum ArmInstructionFormat {
     TransferPsrToRegister,
     TransferRegisterToPsr,
     TransferRegisterOrImmediateValueToPsrFlags,
+    SingleDataTransfer,
+    HalfwordDataTransferRegisterOffset,
+    HalfwordDataTransferImmediateOffset,
     BlockDataTransfer,
     SoftwareInterrupt,
-    SingleDataTransfer,
     SingleDataSwap,
-    HalfDataTransferRegisterOffset,
-    HalfDataTransferImmediateOffset,
 }
 
 impl From<u32> for ArmInstructionFormat {
     fn from(instruction: u32) -> ArmInstructionFormat {
         use ArmInstructionFormat::*;
-        if instruction & 0x0F00_0000 == 0x0A00_0000 || instruction & 0x0F00_0000 == 0x0B00_0000 {
+        if instruction & 0x0E00_0000 == 0x0A00_0000 {
             return BranchAndBranchWithLink;
         }
         if instruction & 0x0FFF_FFF0 == 0x012F_FF10 {
             return BranchAndExchange;
+        }
+        if instruction & 0x0F00_0000 == 0x0F00_0000 {
+            return SoftwareInterrupt;
         }
         if instruction & 0x0E00_0010 == 0x0600_0010 {
             return Undefined;
@@ -47,25 +50,21 @@ impl From<u32> for ArmInstructionFormat {
         if instruction & 0x0DBF_F000 == 0x0128_F000 {
             return TransferRegisterOrImmediateValueToPsrFlags;
         }
-
+        if instruction & 0x0C00_0000 == 0x0400_0000 {
+            return SingleDataTransfer;
+        }
+        if instruction & 0x0E40_0F90 == 0x0000_0090 {
+            return HalfwordDataTransferRegisterOffset;
+        }
+        if instruction & 0x0E40_0090 == 0x0040_0090 {
+            return HalfwordDataTransferImmediateOffset;
+        }
+        if instruction & 0x0E00_0000 == 0x0800_0000 {
+            return BlockDataTransfer;
+        }
+        if instruction & 0x0FB0_0FF0 == 0x0100_0090 {
+            return SingleDataSwap;
+        }
         return Undefined;
-    }
-}
-
-impl ArmInstructionFormat {
-    fn is_block_data_transfer(&self, instruction: u32) -> bool {
-        instruction & 0x0E00_0000 == 0x0800_0000
-    }
-
-    fn is_software_interrupt(&self, instruction: u32) -> bool {
-        instruction & 0x0F00_0000 == 0x0F00_0000
-    }
-
-    fn is_single_data_transfer(&self, instruction: u32) -> bool {
-        instruction & 0x0C00_0000 == 0x0400_0000
-    }
-
-    fn is_single_data_swap(&self, instruction: u32) -> bool {
-        instruction & 0x0F80_0FF0 == 0x0100_0090
     }
 }
