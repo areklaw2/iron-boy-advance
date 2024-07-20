511cd6f0b538a5bd6c2333ac4b8c80f0eb0e1afc
Aza Walker <azawalker.jr@gmail.com>
2024-07-19 23:08:23 -0400

    working on decode helper functions

--- arm7tdmi/src/arm/mod.rs
+++ arm7tdmi/src/arm/mod.rs
@@ -1,6 +1,10 @@
+use bit::BitIndex;
 use dissassembler::ArmInstructionFormat;
 
-use crate::{Cpu, Instruction};
+use crate::{
+    dissassembler::{Condition, Register},
+    Cpu, Instruction,
+};
 
 pub mod dissassembler;
 pub mod execute;
@@ -10,16 +14,6 @@ pub struct ArmInstruction {
     format: ArmInstructionFormat,
     instruction: u32,
     address: u32,
-}
-
-impl ArmInstruction {
-    pub fn new(format: ArmInstructionFormat, instruction: u32, address: u32) -> ArmInstruction {
-        ArmInstruction {
-            format,
-            instruction,
-            address,
-        }
-    }
 }
 
 impl Instruction for ArmInstruction {
@@ -38,8 +32,40 @@ impl Instruction for ArmInstruction {
     }
 }
 
+impl ArmInstruction {
+    pub fn new(format: ArmInstructionFormat, instruction: u32, address: u32) -> ArmInstruction {
+        ArmInstruction {
+            format,
+            instruction,
+            address,
+        }
+    }
+
+    pub fn cond(&self) -> Condition {
+        self.instruction.bit_range(28..32).into()
+    }
+
+    pub fn rn(&self) -> Register {
+        todo!()
+    }
+}
+
 impl Cpu {
     pub fn arm_execute(&self, instruction: ArmInstruction) {
         todo!("{:?}", instruction)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::dissassembler::Condition;
+
+    use super::{dissassembler::ArmInstructionFormat, ArmInstruction};
+    use ArmInstructionFormat::*;
+
+    #[test]
+    fn get_condition() {
+        let instruction = ArmInstruction::new(BranchAndExchange, 0x8FFF_FFFF, 0);
+        assert_eq!(instruction.cond(), Condition::HI)
     }
 }
