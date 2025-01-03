05487a2a822d4a19aae0e6bd8ccc33efa6239222
Aza Walker <azawalker.jr@gmail.com>
2025-01-02 20:56:13 -0500

    started simple gba struct

---
+++ core/src/gba.rs
@@ -0,0 +1,7 @@
+pub struct GameBoyAdvance {}
+
+impl GameBoyAdvance {
+    pub fn new(rom_buffer: Vec<u8>, bios_buffer: Option<Vec<u8>>, show_logs: bool) -> GameBoyAdvance {
+        GameBoyAdvance {}
+    }
+}
--- platform/dev/src/main.rs
+++ platform/dev/src/main.rs
@@ -1,21 +1,13 @@
-use clap::{arg, Arg, ArgAction, Command};
+use core::gba::GameBoyAdvance;
+use std::{fs::File, io::Read};
+
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
     let arg_matches = Command::new("Iron Boy Advance")
         .about("CLI for Iron Boy Advance")
-        .arg(
-            Arg::new("rom")
-                .short('r')
-                .long("rom")
-                .required(true)
-                .help("Rom file to be loaded"),
-        )
-        .arg(
-            Arg::new("bios")
-                .short('b')
-                .long("bios")
-                .help("Bios file to be loaded"),
-        )
+        .arg(Arg::new("rom").short('r').long("rom").required(true).help("Rom file to be loaded"))
+        .arg(Arg::new("bios").short('b').long("bios").help("Bios file to be loaded"))
         .arg(
             Arg::new("logs")
                 .short('l')
@@ -39,11 +31,27 @@ fn main() {
         )
         .get_matches();
 
-    let rom = arg_matches
-        .get_one::<String>("rom")
-        .expect("Rom is required");
-    let bios = arg_matches.get_one::<String>("bios");
-    let show_log_viewer = arg_matches.get_flag("logs");
-    let show_memory_viewer = arg_matches.get_flag("memory");
-    let show_vram_viewer = arg_matches.get_flag("vram");
+    let rom_path = arg_matches.get_one::<String>("rom").expect("Rom is required");
+    let mut rom = File::open(rom_path).expect("Unable to open rom file");
+    let mut rom_buffer = Vec::new();
+    rom.read_to_end(&mut rom_buffer).expect("Issue while reading rom file");
+
+    let bios_path = arg_matches.get_one::<String>("bios");
+    let bios_buffer = match bios_path {
+        Some(path) => {
+            let mut bios = File::open(path).expect("Unable to open bios file");
+            let mut buffer = Vec::new();
+            bios.read_to_end(&mut buffer).expect("Issue while reading bios file");
+            Some(buffer)
+        }
+        None => None,
+    };
+
+    let show_logs = arg_matches.get_flag("logs");
+    let game_boy_advance = GameBoyAdvance::new(rom_buffer, bios_buffer, show_logs);
+
+    let _show_memory = arg_matches.get_flag("memory");
+    let _show_vram = arg_matches.get_flag("vram");
+
+    //todo build out the windows
 }
