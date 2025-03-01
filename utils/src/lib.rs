8eea8f87e1989a64e8935b9bc53c45202e90eceb
Aza Walker <azawalker.jr@gmail.com>
2025-03-01 14:48:14 -0500

    readded macro

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -10,3 +10,20 @@ pub fn read_file(filename: &PathBuf) -> io::Result<Vec<u8>> {
     file.read_to_end(&mut buffer)?;
     Ok(buffer)
 }
+
+pub mod macros {
+    #[macro_export]
+    macro_rules! get_set {
+        ($name:ident, $set_name:ident, $type:ty) => {
+            pub fn $name(&self) -> $type {
+                self.$name
+            }
+
+            pub fn $set_name(&mut self, value: $type) {
+                self.$name = value
+            }
+        };
+    }
+
+    pub use get_set;
+}
