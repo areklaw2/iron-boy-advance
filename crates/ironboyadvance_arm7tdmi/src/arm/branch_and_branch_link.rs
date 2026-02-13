d5cfb645e09ea93dc832e4dc8b0253afe5082a17
Aza Walker <azawalker.jr@gmail.com>
2026-02-12 22:42:02 -0500

    refactor: branch_and_branch_link

---
+++ crates/ironboyadvance_arm7tdmi/src/arm/branch_and_branch_link.rs
@@ -0,0 +1,48 @@
+use ironboyadvance_utils::bit::BitOps;
+
+use crate::{
+    Condition, CpuAction,
+    cpu::{Arm7tdmiCpu, LR},
+    memory::MemoryInterface,
+};
+
+#[derive(Debug, Clone)]
+pub struct BranchAndBranchWithLink {
+    value: u32,
+    executed_pc: u32,
+}
+
+impl BranchAndBranchWithLink {
+    #[inline]
+    pub fn cond(&self) -> Condition {
+        self.value.bits(28..=31).into()
+    }
+
+    pub fn execute<I: MemoryInterface>(&self, cpu: &mut Arm7tdmiCpu<I>) -> CpuAction {
+        if self.link() {
+            cpu.set_register(LR, cpu.pc() - 4)
+        }
+
+        let offset = ((self.offset() << 8) as i32) >> 6;
+        cpu.set_pc((cpu.pc() as i32).wrapping_add(offset) as u32);
+        cpu.pipeline_flush();
+        CpuAction::PipelineFlush
+    }
+
+    pub fn disassemble(&self) -> String {
+        let cond = self.cond();
+        let link = if self.link() { "L" } else { "" };
+        let expression = self.offset();
+        format!("B{link}{cond} 0x{expression:08X}")
+    }
+
+    #[inline]
+    fn link(&self) -> bool {
+        self.value.bit(24)
+    }
+
+    #[inline]
+    fn offset(&self) -> u32 {
+        self.value.bits(0..=23)
+    }
+}
