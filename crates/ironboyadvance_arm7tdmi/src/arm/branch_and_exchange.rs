b21e677cbe4cc597f217b632fd57a9cb789e6fae
Aza Walker <azawalker.jr@gmail.com>
2026-02-12 22:38:49 -0500

    refactor: branch_and_exchange

---
+++ crates/ironboyadvance_arm7tdmi/src/arm/branch_and_exchange.rs
@@ -0,0 +1,35 @@
+use ironboyadvance_utils::bit::BitOps;
+
+use crate::{Condition, CpuAction, CpuState, Register, cpu::Arm7tdmiCpu, memory::MemoryInterface};
+
+#[derive(Debug, Clone)]
+pub struct BranchAndExchange {
+    value: u32,
+    executed_pc: u32,
+}
+
+impl BranchAndExchange {
+    #[inline]
+    pub fn cond(&self) -> Condition {
+        self.value.bits(28..=31).into()
+    }
+
+    pub fn execute<I: MemoryInterface>(&self, cpu: &mut Arm7tdmiCpu<I>) -> CpuAction {
+        let value = cpu.register(self.rn() as usize);
+        cpu.cpsr_mut().set_state(CpuState::from_bits((value & 0x1) as u8));
+        cpu.set_pc(value & !0x1);
+        cpu.pipeline_flush();
+        CpuAction::PipelineFlush
+    }
+
+    pub fn disassemble(&self) -> String {
+        let cond = self.cond();
+        let rn = self.rn();
+        format!("BX{cond} {rn}")
+    }
+
+    #[inline]
+    fn rn(&self) -> Register {
+        self.value.bits(0..=3).into()
+    }
+}
