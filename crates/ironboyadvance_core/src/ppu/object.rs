08123ee607d712e70f3037ad671f5397a1abd4f6
Aza Walker <azawalker.jr@gmail.com>
2026-05-03 18:07:38 -0400

    feature: finishing object entry

--- crates/ironboyadvance_core/src/ppu/object.rs
+++ crates/ironboyadvance_core/src/ppu/object.rs
@@ -1,6 +1,13 @@
 use bitfields::bitfield;
+use getset::CopyGetters;
 
 use crate::ppu::color::ColorMode;
+
+const OBJECT_SIZES: [[(u16, u16); 4]; 3] = [
+    [(8, 8), (16, 16), (32, 32), (64, 64)], // Square
+    [(16, 8), (32, 8), (32, 16), (64, 32)], // Horizontal
+    [(8, 16), (8, 32), (16, 32), (32, 64)], // Vertical
+];
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum AffineMode {
@@ -118,10 +125,10 @@ pub enum ObjectAttribute1 {
 }
 
 impl ObjectAttribute1 {
-    pub const fn from_raw(raw: u16, is_affine: bool) -> Self {
+    pub const fn from_bits(bits: u16, is_affine: bool) -> Self {
         match is_affine {
-            true => Self::Affine(ObjectAttribute1Affine::from_bits(raw)),
-            false => Self::Normal(ObjectAttribute1Normal::from_bits(raw)),
+            true => Self::Affine(ObjectAttribute1Affine::from_bits(bits)),
+            false => Self::Normal(ObjectAttribute1Normal::from_bits(bits)),
         }
     }
 
@@ -137,5 +144,52 @@ impl ObjectAttribute1 {
             Self::Affine(a) => a.object_size(),
             Self::Normal(n) => n.object_size(),
         }
+    }
+
+    pub const fn into_bits(self) -> u16 {
+        match self {
+            Self::Affine(a) => a.into_bits(),
+            Self::Normal(n) => n.into_bits(),
+        }
+    }
+}
+
+#[bitfield(u16)]
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct ObjectAttribute2 {
+    #[bits(10)]
+    tile_index: u16,
+    #[bits(2)]
+    priority: u8,
+    #[bits(4)]
+    palette_bank: u8,
+}
+
+#[derive(CopyGetters)]
+#[getset(get_copy = "pub")]
+pub struct ObjectEntry {
+    attribute0: ObjectAttribute0,
+    attribute1: ObjectAttribute1,
+    attribute2: ObjectAttribute2,
+}
+
+impl ObjectEntry {
+    pub fn from_oam(bytes: &[u8]) -> Self {
+        let attribute0 = ObjectAttribute0::from_bits(u16::from_le_bytes([bytes[0], bytes[1]]));
+        let is_affine = matches!(attribute0.affine_mode(), AffineMode::Affine | AffineMode::AffineDouble);
+        Self {
+            attribute0,
+            attribute1: ObjectAttribute1::from_bits(u16::from_le_bytes([bytes[2], bytes[3]]), is_affine),
+            attribute2: ObjectAttribute2::from_bits(u16::from_le_bytes([bytes[4], bytes[5]])),
+        }
+    }
+
+    pub fn object_map_pixel_size(&self) -> Option<(u16, u16)> {
+        let shape = self.attribute0.object_shape();
+        if matches!(shape, ObjectShape::Prohibited) {
+            return None;
+        }
+        let size = self.attribute1.object_size() as usize;
+        Some(OBJECT_SIZES[shape as usize][size])
     }
 }
